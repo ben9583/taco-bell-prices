07ff07d445aac1ebe9f596ad9c64d4604ee2e029
Ben Plate <bplate9583@gmail.com>
2023-03-26 14:12:21 -0700

    create index page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
+import styles from './page.module.css'
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+  return (
+    <main className={styles.content}>
+      <h1 className={styles.title}>Taco Bell Price Comparer</h1>
+      <p className={styles.description}>Compare the prices of Taco Bell items across the country.</p>
+
+      <div className={styles.searchUtility}>
+        <input className={styles.input} placeholder="Search by address, city, or zip"></input>
+        <button className={styles.button}>Search</button>
+      </div>
+    </main>
+  )
 }
