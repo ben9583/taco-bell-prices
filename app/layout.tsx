c2f2ace129911928419eb8f54fd4607a47769ff5
Ben Plate <bplate9583@gmail.com>
2023-03-26 13:15:32 -0700

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,10 @@
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Taco Bell Price Comparer</title>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
