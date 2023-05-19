89bf6bba7c6cf78da4a6d9e13b35ad94bd17f9c7
Ben Plate <bplate9583@gmail.com>
2023-05-19 13:02:00 -0700

    add loading icon

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,14 @@
 import styles from './page.module.css'
 
 import { useState } from 'react';
+import LoadingSVG from '../assets/loading.svg';
 import TacoBellStoreBox from '../components/TacoBellStoreBox';
+import Image from 'next/image';
 import type { TacoBellPricesLocation, TacoBellPricesStores } from '../lib/taco-bell-prices/types';
 
 export default function Page() {
   const [storesInput, setStoresInput] = useState('')
+  const [loading, setLoading] = useState(false)
   const [stores, setStores] = useState<TacoBellPricesStores>({
     latitude: 0,
     longitude: 0,
@@ -15,9 +18,12 @@ export default function Page() {
   })
 
   const getStores = async () => {
+    setLoading(true);
+    setStores({latitude: 0, longitude: 0, nearByStores: []})
     const prom = new Promise((resolve, reject) => {
       fetch(`/api/locate?location=${storesInput}`).then(res => {
         if (res.status !== 200) {
+          setLoading(false);
           reject(res);
           return;
         }
@@ -25,11 +31,13 @@ export default function Page() {
         res.json().then((data: TacoBellPricesLocation) => {
           fetch(`/api/findStores?lat=${data.geometry.lat}&lng=${data.geometry.lng}`).then(res2 => {
             if (res2.status !== 200) {
+              setLoading(false);
               reject(res);
               return;
             }
 
             res2.json().then((data2: TacoBellPricesStores) => {
+              setLoading(false);
               setStores(data2);
               resolve(undefined);
             });
@@ -51,6 +59,7 @@ export default function Page() {
         <button className={styles.button} onClick={getStores}>Search</button>
       </div>
 
+      {loading && <Image className={styles.loadingImage} width={64} height={64} src={LoadingSVG} alt="loading..." />}
       <div className={styles.storesContainer}>
         {stores.nearByStores.map(store => <TacoBellStoreBox store={store} key={Number(store.formattedDistance.split(" ")[0]) + Math.random() * 0.00001} />)}
       </div>
