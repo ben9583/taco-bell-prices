6daf4342a67afdf1baba7b0ba0c367f40730e00f
Ben Plate <bplate9583@gmail.com>
2023-05-21 12:34:13 -0700

    fix copyright notice

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,7 +9,7 @@ export default function Footer() {
             <strong>Taco Bell Prices</strong> by <a href="https://ben9583.com">Ben Plate</a> and <a href="https://github.com/ben9583/taco-bell-prices/contributors/">others.</a>
           </p>
           <p>
-            Copyright &copy; Ben Plate, 2023. <a href="https://github.com/ben9583/taco-bell-prices">Source code available here</a>, as stipulated by the license.
+            Copyright &copy; 2023 Ben Plate. <a href="https://github.com/ben9583/taco-bell-prices">Source code available here</a>, as stipulated by the license.
           </p>
         </div>
       </div>
