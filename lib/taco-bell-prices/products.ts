b6ff79552876951169739a2afd700e2669289c36
Ben Plate <bplate9583@gmail.com>
2023-06-16 17:38:32 -0700

    update message

--- lib/taco-bell-prices/products.ts
+++ lib/taco-bell-prices/products.ts
@@ -6,14 +6,14 @@ export const getAveragePrices = async () => {
   const cacheKey = "allProducts";
   await client.connect()
   const cachedData = await client.get(cacheKey);
-  client.disconnect();
+  await client.disconnect();
 
   if (cachedData) {
     console.log("Retrieved data from cache");
     const averagePrices = JSON.parse(cachedData);
     return averagePrices;
   } else {
-    console.warn("WARN: You have not generated the averagePrices.json file! Make a post request to /api/priv/getAllProducts setting the \"token\" header to GET_PRODUCTS_TOKEN from .env to generate it.");
+    console.warn("WARN: You have not generated the national pricing data and saved it to redis! Make a post request to /api/priv/getAllProducts setting the \"token\" header to GET_PRODUCTS_TOKEN from .env to generate it.");
     return [];
   }
 };
